97eefc3ac8394ff2014f64fbdc106a2191759efb
GonzaloHernández <gonzalohernandez@udenar.edu.co>
2022-08-04 09:59:57 -0500

    First components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
-function Hello(props) {
-  return (
-    <h2>This project will show the scheduler interface</h2>
-  )
-}
+import Schedule from "./pages/Schedule"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Hello/>);
+root.render(<Schedule/>);
