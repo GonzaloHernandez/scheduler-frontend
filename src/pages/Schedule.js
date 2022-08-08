38a50712c4a8fb40cc78abb801602b4c5cb9a2b6
Gonzalo Hernández <gonzalohernandez@udenar.edu.co>
2022-08-08 10:05:20 -0500

    24 houres (auto)

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -4,17 +4,21 @@ import Cell from "./Cell"
 
 // import axios from "axios"
 
-const days      = [ "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday" ]
-const houres    = [ "06 - 07","07 - 08","08 - 09","09 - 10","10 - 11","11 - 12","12 - 01","01 - 02","02 - 03",
-                    "03 - 04","04 - 05","05 - 06","06 - 07","07 - 08","08 - 09","09 - 10"]
+const days  = [ "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday" ]
+const cells = []
 
 class Schedule extends React.Component {
-    state = {
-        session: "",
-        dragging: false,
-        select: true,
-        movement: false,
-        previous: false,
+    //-------------------------------------------------------------
+    constructor(props) {
+        super(props)
+        for (let i=0; i<48; i++) cells[i] = i
+        this.state = {
+            session: "",
+            dragging: false,
+            select: true,
+            movement: false,
+            previous: false,
+        }
     }
     //-------------------------------------------------------------
     componentDidMount() {
@@ -33,26 +37,46 @@ class Schedule extends React.Component {
         const data = "0011110011110000000011000011000000000000000000000000000000000000001100000000110000000000000000000000000000000000"
         let matrix = Array(7).fill().map(()=>Array(16).fill())
         let i=0
-        for (let d=0; d<7; d++) {
+        for (let d=0; d<days.length; d++) {
             for (let h=0; h<16; h++) {
                 matrix[d][h] = parseInt(data[i++])
             }
         }
 
+        let houres = []
+        // for (let i=0; i<=24; i++) houres[i] = i.toString()+":00";
+
+        houres[0] = "12:00 AM"
+        for (let i= 1; i<12; i++) {
+            let formatted = i.toLocaleString('en-US',{
+                minimumIntegerDigits: 2,
+                useGrouping: false
+            })
+            houres[i] = formatted+":00 AM";
+            houres[i+12] = formatted+":00 PM";
+        }
+        houres[12] = "12:00 PM"
+        houres[24] = "12:00 AM"
+
         return (
             <table>
                 <tr>
-                    <td></td>
+                    <td rowSpan={50}>
+                        <table>
+                            {houres.map(h=><tr><td align="Right" className="houres">{h}</td></tr>)}
+                        </table>
+                    </td>
                     {days.map(d => <td>{d}</td>)}
                 </tr>
-                {houres.map( (h,hi) => <tr>
-                    <td>{h}</td>
+                {cells.map( (c,ci) => <tr>
+
                     {days.map((d,di) => <td>
                         <Cell
-                            selected={matrix[di][hi]?true:false} 
+                            selected={matrix[di][ci]?true:false} 
                             handler={this}/>
                     </td>)}
                 </tr>)}
+                <tr><td></td></tr>
             </table>
         )
     }
