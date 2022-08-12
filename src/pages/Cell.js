46662f4b44c9ef357c5d38c06c87d0cdb6f2f76f
Gonzalo Hernández <gonzalohernandez@udenar.edu.co>
2022-08-12 13:31:46 -0500

    Changing data structure

--- src/pages/Cell.js
+++ src/pages/Cell.js
@@ -4,24 +4,36 @@ class Cell extends React.Component {
     //---------------------------------------------------
     constructor(props) {
         super(props)
+
         this.state = {
             handler: props.handler,
             r: props.r,
             c: props.c,
+            value: 0
         }
     }
     //---------------------------------------------------
     render() {
-        const [r,c] = [this.state.r, this.state.c]
-        const handler = this.state.handler
-        const starting = handler.state.starting
-        const size = handler.state.size
-        let   data = handler.state.data
+        const [r,c]     = [this.state.r, this.state.c]
+        const handler   = this.state.handler
+        const starting  = handler.state.starting
+        const size      = handler.state.size
+        let   data      = handler.state.data
+        const firstdate = handler.firstdate
+        
+        const currentdate   = new Date(firstdate)
+        currentdate.setDate(firstdate+c)
+        const [y,m,d]   = [currentdate.getFullyear(),currentdate.getMonth(),currentdate.getDate()]
+        const h         = starting/2+r
+
+        const txtdate   = (y + "-" + ("0"+m).slice(-2) + "-" + ("0"+d).slice(-2) + "T" + ("0"+h).slice(-2) + ":" + (m?"00":"30") + ":00")
+        const value     = Date.parse(txtdate)/100000
+        this.setState({value: value})
 
         return (
             <td
                 className={
-                    data[c][r+starting]?
+                    data.find(v=>v===value)?
                         "cell-on":
                     (r+starting)%4===0||((r+starting)-1)%4===0?"cell-off2":"cell-off1"}
                 onWheel={(e)=>{
@@ -38,7 +50,7 @@ class Cell extends React.Component {
                         handler.setState({
                             dragging: true,
                             movement: false,
-                            previous: data[c][r+starting]
+                            previous: data.find(v=>v===value)?true:false //data[c][r+starting]
                         })
                     }
                 }}
@@ -46,12 +58,23 @@ class Cell extends React.Component {
                     handler.setState({select:!e.shiftKey})
                     handler.setState({movement:true})
                     if (handler.state.dragging) {
-                        data[c][r+starting] = handler.state.select
+                        //data[c][r+starting] = handler.state.select
+                        if (!e.shiftKey) {
+                            if (!data.find(v=>v===value)) {
+                                data.push(value)
+                            }
+                        } else {
+                            data = data.filter(v=>v!=value)
+                        }
                     }
                 }}
                 onMouseUp={(e)=>{
                     if (!handler.state.movement) {
-                        data[c][r+starting] = !handler.state.previous
+                        if (handler.state.previous)
+                            data = data.filter(v=>v!=value)
+                        else
+                            data.push(value)
+                        // data[c][r+starting] = !handler.state.previous   //****************** im here */
                     }
                     handler.setState({dragging:false, movement:false})
                 }}
--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -15,29 +15,18 @@ class Schedule extends React.Component {
         //     console.log("Recovery failure")            
         // })
 
-        const info =
-            "000000000000000011111100000000001100000000000000" +
-            "000000000000000011000000000000000000000000000000" +
-            "000000000000000000000000000000000000000000000000" +
-            "000000000000000000111100000011110000000000000000" +
-            "000000000000000000000000000011110000000000000000" +
-            "000000000000000000000000000000000000000000000000" +
-            "000000000000000000000000000000000000000000000000"
+        const info ="16603704,16603758,16603740,16603794,16603776,16603830,16603812,16603866,16603848,16603902,16603884,16603938,16603920,16603974,16603956,16604010,16603992,16604046,16604028,16604082,16604064,16604118,16604100,16604154,16604136,16604190,16604172,16604226"
+            
+        let array = []
+        info.split(",").map( (t,i) => array[i]=parseInt(t))
 
-        let matrix = Array(48).fill().map(()=>Array(7).fill())
-
-        let i=0
-        for (let c=0; c<7; c++) {
-            for (let r=0; r<48; r++) {
-                matrix[c][r] = parseInt(info[i++])
-            }
-        }
+        const today = new Date()
 
         this.state={
-            data: matrix,
+            data: array,
             starting: 14,
             size: 30,
-            today: new Date(),
+            firstdate: (new Date(today)).setDate(today.getDate()-today.getDay() + 1),
             // For selection activity
             dragging: false,
             select: true,
@@ -47,15 +36,15 @@ class Schedule extends React.Component {
     }
     //----------------------------------------------------
     render() {
-        const starting = this.state.starting
-        const size = this.state.size
-        const today = this.state.today
-        const rows = Array.from(Array(size).keys())
-        const days = ["MON","TUE","WED","THU","FRI","SAT","SUN"]
-        const months = ["Juanuary","February","March","April","May","June","July",
-                        "August","September","Octuber","November","December"]
-        const firstday = today.getDate() - today.getDay() + 1
-        const lastday = (new Date(today.getFullYear(), today.getMonth(), 0)).getDate()
+        const starting  = this.state.starting
+        const size      = this.state.size
+        const firstdate = this.state.firstdate
+        const today     = new Date()
+        const rows      = Array.from(Array(size).keys())
+        const days      = [ "MON","TUE","WED","THU","FRI","SAT","SUN" ]
+        const months    = [ "Juanuary","February","March","April","May","June","July",
+                            "August","September","Octuber","November","December" ]
+        const lastday   = (new Date(firstdate.getFullYear(), firstdate.getMonth(), 0)).getDate()
 
         const houres = []
         houres[0] = "12:00 AM"
@@ -72,9 +61,9 @@ class Schedule extends React.Component {
 
         let numberdays = []
         for (let i=0,n=1; i<7; i++) {
-            if (firstday+i<=lastday)
-                numberdays[i] = firstday+i
-            else if (firstday+i===lastday+i)
+            if (firstdate.getDate()+i<=lastday)
+                numberdays[i] = firstdate.getDate()+i
+            else if (firstdate.getDate()+i===lastday+i)
                 numberdays[i] = n++
             else
             numberdays[i] = n++
@@ -91,7 +80,7 @@ class Schedule extends React.Component {
                 {/***************** Subtitile **************/}
                 <tr><th></th>
                     {days.map((t,ti)=>
-                        <th className={ti+firstday===today.getDate()?"today":""}>
+                        <th className={ti+firstdate.getDate()===today.getDate()?"today":""}>
                             {t} {numberdays[ti]}
                         </th>
                     )}
@@ -106,7 +95,7 @@ class Schedule extends React.Component {
                         {houres[(r+starting)/2]}
                     </td>
                     {days.map((c,ci)=>{
-                        return <Cell handler={this} r={r} c={ci}/>
+                        return <Cell handler={this} r={ri} c={ci}/>
                     })}
                     </tr>
                 })}
